dacad3e8bf72ae718a3109ce4f37ffd6bcb6a90c
sudeep87 <sudeep.jhs@gmail.com>
2018-07-04 20:38:55 +0530

    thrid commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-for(int i=0; i<10;i++){
+for(int i=0; i<20;i++){
         System.out.println("Hello, World");}
     }
 
