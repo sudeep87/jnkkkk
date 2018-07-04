a2964beed57220123590abc0b805ce5b65665701
sudeep87 <sudeep.jhs@gmail.com>
2018-07-04 20:32:24 +0530

    sec commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+for(int i=0; i<10;i++){
+        System.out.println("Hello, World");}
+    }
+
+}
